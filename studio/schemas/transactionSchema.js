bf09f6f86a8863d029bbcc0d42727aba827250b5
Junaid Malik <junmalik109@gmail.com>
2022-04-24 08:49:29 +0100

    sanity project config added

---
+++ studio/schemas/transactionSchema.js
@@ -0,0 +1,9 @@
+export const transactionSchema = {
+    name: 'transactions',
+    title: 'Transactions',
+    type: 'document',
+    fields: [
+        name: 'txHash',
+        title: 'Transaction Hash',
+    ]
+}
